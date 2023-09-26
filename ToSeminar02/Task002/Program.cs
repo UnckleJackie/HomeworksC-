69244d15965065afbd2dfa05703410bf23a841c8
Puchkov Eugene <ivc037@gmail.com>
2023-09-26 18:57:52 +0300

    Added Homework to Seminar02

---
+++ ToSeminar02/Task002/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая выводит третью
+// цифру заданного числа или сообщает, что третьей цифры нет.
+// Третья цифра - это будут сотни, стало быть будем иметь дело с x / 100
+// Также необходимо выполнить условие Если меньше 100, то пишем, что нет цифры
+
+void ThirdDigit()
+{
+    System.Console.WriteLine("Ай, здраствуй дарагой, да, Хочищь фокус тебе пакажу. Введи по-братски число какое-нибудь: ");
+    int num = Convert.ToInt32(System.Console.ReadLine());
+    System.Console.WriteLine("Спасибо, слющий! Щас у тебя челюсть атпадет, те гаварю да!");
+    System.Console.WriteLine();
+    int thDig = num / 100;
+
+     if (num >= 100) //либо if (thDig = 0, то меняем сценарии местами)
+     {
+         System.Console.WriteLine($"Ваааа, прикинь, третья цифра тваего числа {thDig} Крутяк, да? Сотка гони эээ ");
+         System.Console.WriteLine();
+         System.Console.WriteLine();
+     }
+     else     
+     {
+         System.Console.WriteLine("Аааай, вацок, слющий, зачем такой маленький число сказаль да, там нет третий цифра, ц-ц-ц...");
+         System.Console.WriteLine();
+         System.Console.WriteLine();
+     }
+
+}
+
+ThirdDigit();
---
+++ ToSeminar02/Task003/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на
+// вход цифру, обозначающую день недели, и проверяет,
+// является ли этот день выходным.
+
+bool Weekend (int num)
+{
+    if(num == 6 & num == 7)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+System.Console.Write("Input day of the week: ");
+int num = Convert.ToInt32(Console.ReadLine());
+bool result = Weekend(num);
+System.Console.WriteLine(result);
